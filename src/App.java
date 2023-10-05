7ed3c9ed45949e60d1175cee21be85c95def615c
UlissesRD <pedroulisses@advocareis.com.br>
2023-10-05 19:11:13 -0300

    Adicionando método reverseArrayUsingStack()

--- src/App.java
+++ src/App.java
@@ -1,36 +1,31 @@
-
 public class App {
-
     public static void main(String[] args) {
-
+        // Criar uma instância da pilha
         StackOfInterger stack = new StackOfInterger();
 
-        // Adicionando elementos à pilha
-        stack.push(10);
-        stack.push(20);
-        stack.push(30);
-        stack.push(40);
-        stack.push(50);
-        stack.push(60);
-        stack.push(70);
-        stack.push(80);
+        // Inserir elementos na pilha
+        stack.push(1);
+        stack.push(2);
+        stack.push(3);
 
-        // Imprimindo o tamanho da pilha
+        // Imprimir o tamanho da pilha
         System.out.println("Tamanho da pilha: " + stack.size());
 
-        // Verificando se a pilha está vazia
+        // Imprimir o topo da pilha
+        System.out.println("Topo da pilha: " + stack.top());
+
+        // Verificar se a pilha está vazia
         System.out.println("A pilha está vazia? " + stack.isEmpty());
 
-        // Imprimindo o elemento no topo da pilha
-        System.out.println("Elemento no topo da pilha: " + stack.top());
+        // Inverter um array usando a pilha
+        Integer[] arr = {4, 5, 6, 7};
+        Integer[] reversedArr = StackOfInterger.reverseArrayUsingStack(arr);
 
-        // Removendo elementos da pilha
-        System.out.println("Removendo elementos da pilha:");
-        while (!stack.isEmpty()) {
-            System.out.println(stack.pop());
+        // Imprimir o array invertido
+        System.out.print("Array invertido: ");
+        for (int num : reversedArr) {
+            System.out.print(num + " ");
         }
-
-        // Verificando se a pilha está vazia após a remoção
-        System.out.println("A pilha está vazia? " + stack.isEmpty());
+        System.out.println();
     }
 }
